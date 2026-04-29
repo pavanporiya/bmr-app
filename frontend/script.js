4ccb1b9ea6cc408670e6037fad1625b8776a5337
Pavan Poriya <pavnporiya303@gmail.com>
2026-04-29 00:12:05 +0000

    Corrected mistake of api

--- frontend/script.js
+++ frontend/script.js
@@ -74,7 +74,7 @@ async function signup() {
     }
 
     try {
-        const res = await fetch("http://https://bmr-backend-no2v.onrender.com/signup", {
+        const res = await fetch("https://bmr-backend-no2v.onrender.com/signup", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
@@ -108,7 +108,7 @@ async function login() {
     }
 
     try {
-        const res = await fetch("http://https://bmr-backend-no2v.onrender.com/login", {
+        const res = await fetch("https://bmr-backend-no2v.onrender.com/login", {
             method: "POST",
             mode: "cors",
             headers: { "Content-Type": "application/json" },
@@ -179,7 +179,7 @@ async function calculateBMR() {
     }
 
     try {
-        const res = await fetch("http://https://bmr-backend-no2v.onrender.com/calculate", {
+        const res = await fetch("https://bmr-backend-no2v.onrender.com/calculate", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -204,7 +204,7 @@ async function calculateBMR() {
 // 📊 HISTORY
 async function loadHistory() {
     try {
-        const res = await fetch("http://https://bmr-backend-no2v.onrender.com/history", {
+        const res = await fetch("https://bmr-backend-no2v.onrender.com/history", {
             headers: { "Authorization": "Bearer " + token }
         });
 
@@ -237,7 +237,7 @@ function deleteRecord(id) {
     const resultDiv = document.getElementById("result");
     resultDiv.innerText = "Deleting...";
 
-    fetch(`http://https://bmr-backend-no2v.onrender.com/delete/${id}`, {
+    fetch(`https://bmr-backend-no2v.onrender.com/delete/${id}`, {
         method: "DELETE",
         headers: { "Authorization": "Bearer " + token }
     }).then(() => {
